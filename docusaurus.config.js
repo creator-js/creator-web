4cf1526f232e63a8a75f2df8ffd6cd90d83989ee
Michal Kutateladze <michaeladze@returns.co>
2022-11-27 14:04:01 +0200

    slogan

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'CreatorJS',
-  tagline: 'Automate boilerplate code generation',
+  tagline: 'Automate boilerplate code creation',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
